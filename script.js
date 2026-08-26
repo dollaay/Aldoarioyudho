5a98e325a4deddb2e7706b7b0c913c01c72bd593
dollaay <yudhoaldoario577@gmail.com>
2026-08-26 22:24:05 +0700

    porto

---
+++ script.js
@@ -0,0 +1,15 @@
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.querySelector(".nav-menu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
